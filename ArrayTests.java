a0e7792b85a63c2aa135b185cdf2fa2ccbef2c24
Divinity <147002921+divine9223@users.noreply.github.com>
2023-10-25 15:14:32 -0700

    fixed reverseInPlace and Reversed

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,19 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlace2() {
+    int[] input1 = {1,2,3,4};
+    int[] input2 = {4,3,2,1};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(input2, input1);
+  }
+
+  @Test
+  public void testReversed2(){
+    int[] input1 = {1,2,3,4};
+    int[] input2 = {4,3,2,1};
+    assertArrayEquals(input2, ArrayExamples.reversed(input1));
+  }
 }
